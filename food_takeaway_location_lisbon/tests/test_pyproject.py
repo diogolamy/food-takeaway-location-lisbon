ab3f2ce8506e05ce100314b01ba61a7d687b8d6b
diogolamy <diogolamy1999@gmail.com>
2025-08-07 13:31:40 +0100

    add a test to pyproject

---
+++ food_takeaway_location_lisbon/tests/test_pyproject.py
@@ -0,0 +1,9 @@
+'''
+Test the pyproject.toml file
+'''
+
+def test_import_main_package():
+    try:
+        import food_takeaway_location_lisbon
+    except ImportError:
+        assert False, "Failed to import food_takeaway_location_lisbon"
